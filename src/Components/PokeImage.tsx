f54768e5e6c828dd39994aed2a09411fd4eacabd
eclonghurst <emily.longhurst3@sky.uk>
2023-10-12 14:35:26 +0100

    loading added and randomisation fixed

--- src/Components/PokeImage.tsx
+++ src/Components/PokeImage.tsx
@@ -6,42 +6,55 @@ import "../CSS/PokeImage.css";
 function PokeImage() {
   // strongly typed state
   const [pokemonImage, setPokemonImage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
 
   const getRandomPokemon = async () => {
     const api = new PokemonClient();
-    // getting the total number of pokemon by the length of the api's list
-    const pokemonList = await api.listPokemons(900);
-    const totalPokemon = pokemonList.results.length;
+    const randomIndex = Math.floor(Math.random() * 898) + 1; // 1 to 898
+    const data = await api.getPokemonById(randomIndex);
+    console.log(`Fetching Pokémon at index ${randomIndex}: ${data.name}`);
 
-    const randomPokemon = Math.floor(Math.random() * totalPokemon);
-    const randomPokemonName = pokemonList.results[randomPokemon].name;
-    return randomPokemonName;
+    return data.id;
+  };
+
+  const fetchPokemonImage = async (
+    retryCount: number = 10
+  ): Promise<undefined> => {
+    if (retryCount <= 0) {
+      throw new Error("Too many attempts");
+    }
+    const api = new PokemonClient();
+
+    const data = await api.getPokemonById(await getRandomPokemon());
+    if (data.sprites.front_default) {
+      // type of pokemonImage is default string so will not accept a null value - this assigns an empty string if null
+      setPokemonImage(data.sprites.front_default || "");
+      setLoading(false);
+      console.log(data.name);
+    } else {
+      console.log("sprite missing for ${randomPokemonName}. Retrying");
+      return await fetchPokemonImage(retryCount - 1);
+    }
   };
 
   useEffect(() => {
-    const fetchPokemonImage = async () => {
-      const api = new PokemonClient();
-      try {
-        const data = await api.getPokemonByName(await getRandomPokemon());
-        // type of pokemonImage is default string so will not accept a null value - this assigns an empty string if null
-        setPokemonImage(data.sprites.front_default || "");
-        console.log(data.name);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchPokemonImage();
   }, []);
 
   return (
     <>
       <div className="pokeimage-container">
-        <img
-          className="pokeimage"
-          src={pokemonImage}
-          alt="Hidden Pokemon"
-          width={"200px"}
-        />
+        {loading ? (
+          <p>Loading ...</p>
+        ) : (
+          <img
+            className="pokeimage"
+            src={pokemonImage}
+            alt="Hidden Pokemon"
+            width={"200px"}
+            onLoad={() => setLoading(false)}
+          />
+        )}
       </div>
     </>
   );
