0479e9af2509056f6e460b1af5302cc6b5d8fc41
eclonghurst <emily.longhurst3@sky.uk>
2023-10-13 09:29:52 +0100

    win message with timeout

--- src/Components/GuessBar.tsx
+++ src/Components/GuessBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Pokemon } from "../Types";
 
 function GuessBar({
@@ -9,6 +9,24 @@ function GuessBar({
   onCorrectGuess: () => void;
 }) {
   const [guess, setGuess] = useState<string>("");
+  const [hasWon, setHasWon] = useState<boolean>(false);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+    // If the guess is correct, set a timeout to reset the win message after 3 seconds
+    if (hasWon) {
+      timerId = setTimeout(() => {
+        setHasWon(false);
+      }, 1000);
+    }
+    // Clean up timeout if component is unmounted before timeout is reached
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+    };
+  }, [hasWon]);
+
   function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
     setGuess(event.target.value);
   }
@@ -17,6 +35,7 @@ function GuessBar({
     console.log("The user guessed: " + guess);
     if (guess.toLowerCase() === pokemon.name?.toLowerCase()) {
       console.log("you won");
+      setHasWon(true);
       onCorrectGuess();
       setGuess("");
     } else {
@@ -38,6 +57,7 @@ function GuessBar({
           Guess
         </button>
       </div>
+      {hasWon && <h2>You got it!</h2>}
     </>
   );
 }
